d9ba687347347f9894e8ab517f4c700c901769dc
Franco Lerch <franco.lerch@gmail.com>
2018-12-09 16:36:31 -0300

    removed comment

--- Class4/Class4/src/Application.cpp
+++ Class4/Class4/src/Application.cpp
@@ -4,12 +4,11 @@
 
 int main()
 {
-	//srand(time(NULL));
-
 	cpp_class4_test::testRandomPrimeInteger();
 	cpp_class4_test::testGetLanguageCreator();
 	cpp_class4_test::testFindFirstOccurence();
 	cpp_class4_test::testSortByType();
+
 	std::cin.get();
 	return 0;
 };
